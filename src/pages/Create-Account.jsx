46d9835101580c4be89cb7b08689f7b626384af8
Alieze-Ali <aliezeali77@gmail.com>
2024-10-25 15:26:41 -0700

    added components folder and login and create account set up pages

---
+++ src/pages/Create-Account.jsx
@@ -0,0 +1,12 @@
+// Create Account page
+import React from 'react';
+
+const CreateAccountPage = () => {
+    return (
+        <div>
+            <h1>Create Account Page</h1>
+            {/* Our Create Account form will go here */}
+        </div>
+    );
+};
+export default CreateAccountPage; 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+// Login Page
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            {/* Our Login form will go here */}
+        </div>
+    );
+};
+
+export default Login; 
