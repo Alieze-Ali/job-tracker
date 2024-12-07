6c9c9f01fc8f246dac072d10d450550c3d13a4c0
Alieze-Ali <aliezeali77@gmail.com>
2024-12-07 07:45:12 -0800

    added login form and css
    git commit -m 'added login form and css page'
    
    git commit -m 'added login form and css'

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,9 @@
 // Login Page
 import React from 'react';
 import googleButton from '../assets/google_signin_buttons/web/1x/btn_google_signin_dark_pressed_web.png'
+import { Link } from 'react-router-dom';
+import './Login.css';
+
 
 const Login = () => {
 
@@ -17,13 +20,35 @@ const Login = () => {
     }
 
     return (
+<<<<<<< HEAD
         <div>
             <h2>Login Page</h2>
             <h3>Google OAuth!</h3>
             <button className="btn-auth"  type="button" onClick={()=> auth()}>
                 <img className="btn-auth-img" src={googleButton} alt='google sign in'/>
             </button>
+=======
+        <div className="login-container">
+            <div className="login-box">
+                <h2>Login Page</h2>
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" id="username" placeholder="Enter your username" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" name="password" placeholder="Enter your password" />
+                        </div>
+                        <button type = "submit" className="login-button">Login</button>     
+                </form>
+                <p className="register-prompt">
+                    Don't have an account? <Link to="/create-account">Register</Link>
+                </p>
+            </div>
+>>>>>>> 14608a1 (added login form and css)
         </div>
+        
     );
 };
 
