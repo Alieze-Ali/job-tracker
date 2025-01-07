d4df87b055a2998af96200044b4f5c3cb96febf8
Samantha G <antashma@gmail.com>
2025-01-07 13:33:12 -0500

    remove empty middleware folder

--- backend/controllers/onboarding-controller.js
+++ backend/controllers/onboarding-controller.js
@@ -34,18 +34,6 @@ const login =  async (req, res) => {
 
 
 
-
-
-
-function hashPassword(password) {
-    const saltRounds = 10;
-    bcrypt.genSalt(saltRounds, function(err, salt) {
-        bcrypt.hash(password, salt, function(err, hash) {
-            // Store hash in your password DB.
-        });
-    });
-}
-
 module.exports = {
     register,
     login
